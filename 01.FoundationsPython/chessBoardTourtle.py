1db85b911dac05e13763372b674cfcfe5d5a9870
Aleksandyr <aleksandyr.kamenov@gmail.com>
2016-03-27 20:28:28 +0300

    chess board modified

--- 01.FoundationsPython/chessBoardTourtle.py
+++ 01.FoundationsPython/chessBoardTourtle.py
@@ -1,5 +1,6 @@
 import turtle
 side = 50
+deg = 90
 x = -300
 y = 150
 counter = 0
@@ -8,25 +9,26 @@ turtle.penup()
 turtle.goto(x,y)
 turtle.pendown()
 
-for j in range(8):
-	for i in range(8):
-	    if ((i + counter) % 2) == 0:
-	    	turtle.begin_fill()    
-	    turtle.forward(side)
-	    turtle.left(90)
-	    turtle.forward(side)
-	    turtle.left(90)
-	    turtle.forward(side)
-	    turtle.left(90)
-	    turtle.forward(side)
-	    turtle.left(90)
-	    turtle.end_fill()
+for row in range(8):
+	for col in range(8):
+	    if ((col + counter) % 2) == 0:
+	    	turtle.begin_fill()   
+
+	    for rect in range(4):
+	    	turtle.forward(side)
+	    	turtle.left(deg)	    
+
+	    if ((col + counter) % 2) == 0:
+	    	turtle.end_fill()     
+	    
 	    turtle.forward(side)
 	
 	y -= 50
+
 	turtle.penup()
 	turtle.goto(x,y)
 	turtle.pendown()
+
 	counter += 1
 
 turtle.exitonclick()
