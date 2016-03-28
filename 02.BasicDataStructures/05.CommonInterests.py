345831c30f68148f11a6819a45ab0d6d8e3991a9
Aleksandyr <aleksandyr.kamenov@gmail.com>
2016-03-28 20:21:25 +0300

    common interests

---
+++ 02.BasicDataStructures/05.CommonInterests.py
@@ -0,0 +1,7 @@
+ivan = ['пушене', 'пиене', 'тия три неща', 'коли', 'facebook', 'игри', 'разходки по плажа', 'скандинавска поезия']
+maria = ['пиене', 'мода', 'facebook', 'игри', 'лов със соколи', 'шопинг', 'кино']
+
+ivanSet = set(ivan)
+mariaSet = set(maria)
+
+print(ivanSet.intersection(mariaSet))
